b7d4d7c2f2f30608c436e0e4ed7893cf0cc862f2
deadthings <33961641+deadthings@users.noreply.github.com>
2020-09-16 22:05:26 -0400

    Update getxuezbootstrap.sh

--- getxuezbootstrap.sh
+++ getxuezbootstrap.sh
@@ -4,5 +4,5 @@ fileid="1KQFV7MYDEwOy_gLo80fJaXDSiXymuf-V"
 filename="bootstrap.rar"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
-unzip ${filename}
+unrar x ${filename}
 rm ${filename} cookie
